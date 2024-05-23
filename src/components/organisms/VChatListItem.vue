8c1a5fd8a33cba45ed381962043e80a600af3e8b
iamrcayna <i.am.arcayna@gmail.com>
2024-05-23 16:41:53 +0800

    added VChatListItem component

---
+++ src/components/organisms/VChatListItem.vue
@@ -0,0 +1,27 @@
+<template>
+	<div class="flex items-center p-3 gap-4 hover:bg-indigo-500/10 cursor-pointer transition-all">
+		<VAvatar/>
+		<VTextGroup class="flex-1" title="John Doe" text="lets go eat dinner dude"/>
+		<div class="flex items-center flex-col gap-2">
+			<p class="text-sm">Just now</p>
+			<VBadge :count="12"/>
+		</div>
+	</div>
+</template>
+
+<script lang="ts" setup>
+import VAvatar from "@/components/atoms/VAvatar.vue"
+import VBadge from "@/components/atoms/VBadge.vue"
+import VTextGroup from "@/components/molecules/VTextGroup.vue"
+import type { Channel } from "@/types/Channel"
+
+
+
+// const props = defineProps<{
+// 	item: Channel;
+// }>()
+
+const emits = defineEmits<{
+	open: [value: string]
+}>()
+</script>

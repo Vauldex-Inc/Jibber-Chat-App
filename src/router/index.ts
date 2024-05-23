8c710ac0815dc33d2695871dd888256e8c717604
gmmortera <gfmmortera@gmail.com>
2024-05-23 14:18:21 +0800

    added a new page file and created a register route

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import index from '@/components/pages/index.vue'
 import TheLogin from '@/components/pages/TheLogin.vue'
+import TheRegister from '@/components/pages/TheRegister.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,6 +15,11 @@ const router = createRouter({
       path: '/login',
       name: 'login',
       component: TheLogin
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: TheRegister
     }
   ]
 })

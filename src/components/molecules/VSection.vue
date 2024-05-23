9730e95226f27ee61c10df68b5c7d9c839c9a504
iamrcayna <i.am.arcayna@gmail.com>
2024-05-23 14:59:15 +0800

    added VSection component

---
+++ src/components/molecules/VSection.vue
@@ -0,0 +1,30 @@
+<template>
+	<div>
+		<div class="flex items-center justify-between mb-2">
+			<p class="capitalize font-semibold text-gray-600 dark:text-gray-400">{{title}}</p>
+			<VButton 
+				size="sm"
+				@click="emits('click')" 
+				class="capitalize text-indigo-600 dark:text-indigo-500 font-semibold hover:bg-indigo-100 rounded-md" 
+				v-if="actionButton">
+				{{ actionButton }}
+			</VButton>
+		</div>
+		<slot/>
+	</div>
+</template>
+
+<script lang="ts" setup>
+import VButton from "@/components/atoms/VButton.vue"
+
+interface VSectionProps {
+	title: string;
+	actionButton?: string;
+}
+
+defineProps<VSectionProps>()
+
+const emits = defineEmits<{
+	click: [];
+}>()
+</script>

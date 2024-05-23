6dfe13861bde1a00ee0414df742e1af595cb5e3a
iamrcayna <i.am.arcayna@gmail.com>
2024-05-23 14:57:05 +0800

    added VAvatar component

---
+++ src/components/atoms/VAvatar.vue
@@ -0,0 +1,49 @@
+<template>
+	<div class="relative inline-block">
+		<img 
+			:src="image? image : './src/assets/images/default-avatar.svg'"
+			:class="[imageClass]"
+			class="aspect-square relative z-0">
+		<div 
+			:class="[statusClass]"
+			class="absolute bottom-0 right-0 h-5 aspect-square z-10 rounded-full border-[3px] border-gray-100 translate-x-1 translate-y-1">
+		</div>
+	</div>
+</template>
+
+
+<script lang="ts" setup>
+import {computed} from "vue"
+
+const imageClass = computed(() => {
+  switch (props.size) {
+    case "sm":
+      return "h-10";
+    case "md":
+      return "h-12";
+    case "lg":
+      return "h-16";
+    default:
+      return "h-12";
+  }
+});
+
+const statusClass = computed(() => {
+  switch (props.status) {
+	case "online":
+		return "bg-emerald-500";
+	case "offline":
+		return "bg-red-500";
+    default:
+      return "bg-red-500";
+  }
+})
+
+interface VAvatarProps {
+	image?: string;
+	size?: "sm" | "md" | "lg";
+	status?: "online" | "offline"
+}
+
+const props = defineProps<VAvatarProps>()
+</script>

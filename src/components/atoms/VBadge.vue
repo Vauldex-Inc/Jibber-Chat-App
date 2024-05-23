a4af2bf9b20b11b55de49691c6097b4d17d4fc42
iamrcayna <i.am.arcayna@gmail.com>
2024-05-23 14:55:38 +0800

    added VBadge component

---
+++ src/components/atoms/VBadge.vue
@@ -0,0 +1,19 @@
+<template>
+	<span :class="color? color : 'bg-indigo-600'" class="p-1 rounded-full text-xs text-gray-100 dark:text-gray-200 font-semibold">
+		{{count}}
+	</span>
+</template>
+
+<script lang="ts" setup>
+
+interface VBadgeProps {
+	color?: string;
+	count: number;
+}
+
+defineOptions({
+	inheritAttrs: false
+})
+
+defineProps<VBadgeProps>()
+</script>

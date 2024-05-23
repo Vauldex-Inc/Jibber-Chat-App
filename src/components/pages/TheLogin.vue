321f7fe32f48cf1eaa55d6109739074833b3b884
Chris Dickson C. Molina <cdcmolina@vauldex.com>
2024-05-23 16:45:28 +0800

    added logic for login page

--- src/components/pages/TheLogin.vue
+++ src/components/pages/TheLogin.vue
@@ -10,14 +10,28 @@
 			</div>
 		</section>
 		<section class="bg-white w-1/3 p-8 rounded-lg">
-			<form @submit.prevent class="flex flex-col gap-2">
+			<form @submit.prevent="login" class="flex flex-col gap-2">
 				<h2 class="font-bold text-3xl mb-4">Login</h2>
 				<label for="username" class="text-start text-sm font-bold">Username</label>
-				<input type="text" id="username" class="bg-gray-50 p-0.5 rounded-dm border-2 outline-none border-gray-300 focus:border-blue-400 hover:border-blue-400" />
+				<input v-model="username" type="text" id="username" required class="bg-gray-50 px-5 py-3 rounded-md border-2 outline-none border-gray-300 focus:border-blue-400 hover:border-blue-400" />
 				<label for="password" class="text-start text-sm font-bold">Password</label>
-				<input type="password" id="password" class="bg-gray-50 p-0.5 rounded-dm border-2 outline-none border-gray-300 focus:border-blue-400 hover:border-blue-400"/>
-				<button type="submit" class="bg-blue-800 text-md p-1 text-white rounded-md mt-4">Submit</button>
+				<input v-model="password" type="password" id="password" required class="bg-gray-50 px-5 py-3 rounded-md border-2 outline-none border-gray-300 focus:border-blue-400 hover:border-blue-400"/>
+				<button type="submit" class="bg-blue-800 text-md py-3 px-5 text-white rounded-md mt-4 font-semibold">Submit</button>
 			</form>
+			<p class="mt-1.5">Don't have an account? <RouterLink class="text-blue-800 cursor-pointer" to="/register">Register now</RouterLink></p>
 		</section>
 	</div>
 </template>
+
+<script setup lang="ts">
+import { ref } from "vue"
+import { useRouter, RouterLink } from "vue-router"
+const resource = import.meta.env.VITE_JIBBER_BASE_URL
+
+const router = useRouter()
+const username = ref<string>('')
+const password = ref<string>('')
+
+
+
+</script>

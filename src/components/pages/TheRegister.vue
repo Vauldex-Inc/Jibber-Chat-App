c5fb831f2a36d93b8a4d7d6b004f06abe1c65bcc
gmmortera <gfmmortera@gmail.com>
2024-05-24 13:12:56 +0800

    update the registration script logic

--- src/components/pages/TheRegister.vue
+++ src/components/pages/TheRegister.vue
@@ -9,11 +9,11 @@
 				<p class="text-center text-black text-3xl">Register</p>
 				<p v-if="error" class="text-red-500 text-center text-sm border border-red-600 bg-red-500/10 p-3 rounded-md">{{ error }}</p>
 				<label for="username" class="font-semibold">Username</label>
-				<input id="username" type="text" class="bg-gray-50 px-5 py-3 rounded-md border-2 outline-none border-gray-300 focus:border-indigo-600 hover:border-indigo-600" v-model="username" required />
+				<input id="username" type="text" class="bg-gray-50 px-5 py-3 rounded-md border-2 outline-none border-gray-300 focus:border-indigo-600 hover:border-indigo-600" v-model="formData.username" required />
 				<label for="password" class="font-semibold">Password</label>
-				<input id="password" type="password" class="bg-gray-50 px-5 py-3 rounded-md border-2 outline-none border-gray-300 focus:border-indigo-600 hover:border-indigo-600" v-model="password" required />
+				<input id="password" type="password" class="bg-gray-50 px-5 py-3 rounded-md border-2 outline-none border-gray-300 focus:border-indigo-600 hover:border-indigo-600" v-model="formData.password" required />
 				<label for="confirm" class="font-semibold">Confirm Password</label>
-				<input id="confirm" type="password" class="bg-gray-50 px-5 py-3 rounded-md border-2 outline-none border-gray-300 focus:border-indigo-600 hover:border-indigo-600" v-model="confirmation" required />
+				<input id="confirm" type="password" class="bg-gray-50 px-5 py-3 rounded-md border-2 outline-none border-gray-300 focus:border-indigo-600 hover:border-indigo-600" v-model="formData.confirmation" required />
 				<button type="submit" class="bg-indigo-600 text-md px-5 py-3 text-white rounded-md mt-4" @click="register">Register</button>
 				<p class="text-center">Already have an account? <RouterLink class="text-indigo-600 focus:border-indigo-600 hover:underline cursor-pointer" to="/login">Login Here</RouterLink></p>
 			</form>
@@ -27,54 +27,54 @@
 
 <script setup lang="ts">
 import { RouterLink } from 'vue-router'
-import { ref, watch, computed } from 'vue'
+import { ref, watch } from 'vue'
 import { useFetch } from '@/composables/useFetch.ts'
 
-interface Account {
-	username: string
-	password: string
-}
-
-const username = ref<string>('')
-const password = ref<string>('')
-const confirmation = ref<string>('')
-const error = ref<string>('')
-const account = computed(() => {
-	return { 
-		username: username.value,
-		password: password.value
- 	}
+const formData = ref({
+	username: "",
+	password: "",
+	confirmation: ""
 })
-
-const validate = () => {
-	if(password.value === confirmation.value && password.value !== "" && confirmation.value !== "") {
-		error.value = ""
-	} else {
-		error.value = "Password does not match."
-	}
-}
+const error = ref<string>('')
 
 const register = async () => {
 	try {
 		validate()
 		if(!error.value) {
-				let option = {
+			const response = await useFetch("/users", {
 				method: "POST",
-				body: JSON.stringify(account.value)
+				body: JSON.stringify(formData.value)
+			})
+
+			if(response.status !== 200) {
+				error.value = "Oops! Something went wrong. Try again."
 			}
-			const response = await useFetch("/users", option)
 		}
 	} catch(e) {
-		error.value = "Oops! Something went wrong. Try again."
+		throw new Error(e)
 	} finally {
 		resetInputs()
 	}
 }
 
+const validate = () => {
+	if(formData.value.password !== formData.value.confirmation && 
+	   formData.value.password !== "" && 
+	   formData.value.confirmation !== "") {
+		error.value = "Password does not match."
+	} else if(formData.value.username === "" &&
+	          formData.value.password === "" &&
+	          formData.value.confirmation === "") {
+		error.value = "Input fields are empty."
+	} else {
+		error.value = ""
+	}
+}
+
 const resetInputs = () => {
-	username.value = ""
-	password.value = ""
-	confirmation.value = ""
+	formData.value.username = ""
+	formData.value.password = ""
+	formData.value.confirmation = ""
 	setTimeout(() => {
 			error.value = ""
 		}, 2000)

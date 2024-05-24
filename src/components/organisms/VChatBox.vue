82cb6f6a5aa80e94266049b44f60a1a5e60b266a
iamrcayna <i.am.arcayna@gmail.com>
2024-05-24 10:31:04 +0800

    updated VIconButton

---
+++ src/components/organisms/VChatBox.vue
@@ -0,0 +1,11 @@
+<template>
+	<div>
+		<VInput/>
+		<VIconButton icon=".src/assets/images/close.svg"/>
+	</div>
+</template>
+
+<script setup lang="ts">
+import VInput from "@/components/atoms/VInput.vue"
+import VIconButton from "@/components/atoms/VIconButton.vue"
+</script>

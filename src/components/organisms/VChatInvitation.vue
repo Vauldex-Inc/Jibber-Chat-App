f45f113987ebb7c17346c15af29145eca82250e8
Chris Dickson C. Molina <cdcmolina@vauldex.com>
2024-05-27 14:50:50 +0800

    update chat invite emits

--- src/components/organisms/VChatInvitation.vue
+++ src/components/organisms/VChatInvitation.vue
@@ -15,7 +15,7 @@
 		<hr class="border-0 border-b border-b-indigo-300"/>
 		<footer class="flex gap-4 self-end">
 			<VButton class="border bg-indigo-300 rounded-md text-slate-700" @click="emits('close')">Close</VButton>			
-			<VButton class="border bg-indigo-600 rounded-md text-white">Take a Look</VButton>
+			<VButton class="border bg-indigo-600 rounded-md text-white" @click="emits('view')">Take a Look</VButton>
 		</footer>
 	</div>
 </template>
@@ -27,7 +27,10 @@ const props = defineProps<{
 	description: string
 }>()
 
-const emits = defineEmits<{close: []}>()
+const emits = defineEmits<{
+	close: []
+	view: []
+}>()
 
 </script>
 

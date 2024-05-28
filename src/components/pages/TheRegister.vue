4a7d008a872b2cdceab012b8f6de9b5d724bd801
gmmortera <gfmmortera@gmail.com>
2024-05-28 13:59:02 +0800

    Add username/password validations J3

--- src/components/pages/TheRegister.vue
+++ src/components/pages/TheRegister.vue
@@ -8,45 +8,60 @@
       <form @submit.prevent class="bg-white w-[500px] max-w-full p-8 rounded-lg flex flex-col justify-center gap-3 shadow-md">
         <p class="text-center text-3xl font-medium">Register</p>
         <p v-if="error" class="text-red-500 text-center text-sm border border-red-600 bg-red-500/10 p-3 rounded-md">{{ error }}</p>
-        <label for="username" class="font-semibold">Username</label>
+        <ul v-if="current === 'username'" class="text-red-500 text-xs border bg-gray-100 p-3 rounded-md">
+          <li v-for="validation in usernameValidations">
+            <p :class="{ 'text-green-500': validation.isSuccess }">• {{ validation.message }}</p>
+          </li>
+        </ul>
+        <ul v-if="current === 'password'" class="text-red-500 text-xs border bg-gray-100 p-3 rounded-md">
+          <li v-for="validation in passwordValidations">
+            <p :class="{ 'text-green-500': validation.isSuccess }">• {{ validation.message }}</p>
+          </li>
+        </ul>
+        <label class="font-semibold">Username</label>
         <VInput
           type="text"
           size="lg"
           v-model="formData.username"
-          id="username" 
+          maxlength="20"
           class="bg-gray-50 rounded-md border-2 outline-none border-gray-300 focus:border-indigo-600 hover:border-indigo-600"
+          @focus="current = 'username'"
           required
         />
-        <label for="password" class="font-semibold">Password</label>
+        <label class="font-semibold">Password</label>
         <div class="relative">
           <VInput
             :type="type"
             size="lg"
             v-model="formData.password"
-            id="password" 
             class="bg-gray-50 rounded-md w-full border-2 pr-12 outline-none border-gray-300 focus:border-indigo-600 hover:border-indigo-600"
+            @focus="current = 'password'"
             required
           />
           <VIconButton
             size="sm"
             :icon="icon"
+            tabindex="20"
+            :invert="true"
             class="absolute top-1/2 right-0 -translate-x-1/2 -translate-y-1/2 bg-indigo-600"
             @click="toggle"
           />
         </div>
-        <label for="confirm" class="font-semibold">Confirm Password</label>
+        <label class="font-semibold">Confirm Password</label>
         <div class="relative">
           <VInput
             :type="type"
             size="lg"
             v-model="formData.confirmation"
-            id="confirm" 
             class="bg-gray-50 rounded-md border-2 w-full pr-12 outline-none border-gray-300 focus:border-indigo-600 hover:border-indigo-600"
+            @focus="current = undefined"
             required
           />
           <VIconButton
             size="sm"
             :icon="icon"
+            tabindex="21"
+            :invert="true"
             class="absolute top-1/2 right-0 -translate-x-1/2 -translate-y-1/2 bg-indigo-600"
             @click="toggle"
           />
@@ -62,7 +77,7 @@
         <VButton
           v-else 
           action="button" 
-          class="bg-indigo-600 text-md text-white rounded-md mt-4 flex justify-center"
+          class="bg-indigo-600 text-md text-white rounded-md mt-4 flex justify-center cursor-not-allowed"
           disabled>
           <div class="p-2 mr-3 border-indigo-700/50 border-b-indigo-200 border-4 rounded-full animate-spin">
             <p class="sr-only">Loading</p>
@@ -81,12 +96,17 @@
 
 <script setup lang="ts">
 import { RouterLink, useRouter } from 'vue-router'
-import { ref, watch } from 'vue'
+import { ref, watchEffect } from 'vue'
 import { useFetch } from '@/composables/useFetch.ts'
 import VInput from '@/components/atoms/VInput.vue'
 import VButton from '@/components/atoms/VButton.vue'
 import VIconButton from '@/components/atoms/VIconButton.vue'
 
+interface Validation {
+  message: string
+  isSuccess: boolean
+  regex: RegExp
+}
 
 const formData = ref({
   username: "",
@@ -99,6 +119,18 @@ const visibility = ref<boolean>(false)
 const icon = ref<string>('./src/assets/images/visibility-true.svg')
 const type = ref<"text" | "password">('password')
 const router = useRouter()
+const current = ref<"username" | "password">()
+const passwordValidations = ref<Validation[]>([
+  { message: "Must be at least 8 characters long", isSuccess: false, regex: /^.{8,20}$/ },
+  { message: "Must contain at least 1 uppercase letter", isSuccess: false, regex: /[A-Z]/ },
+  { message: "Must contain at least 1 digit", isSuccess: false, regex: /\d/ },
+  { message: "Must contain at least 1 special character", isSuccess: false, regex: /[!@#$%^&*()\\[\]{}\-_+=~|:;"'<>,.?/ ]/ }
+])
+const usernameValidations = ref<Validation[]>([
+  { message: "Must be at least 8 characters long", isSuccess: false, regex: /^.{8,20}/ },
+  { message: "Must contain letters or numbers", isSuccess: false, regex: /^[a-zA-Z0-9]+$/ },
+  { message: "Must not have @ character", isSuccess: false, regex: /^[^@]+$/ }
+])
 
 const register = async () => {
   try {
@@ -112,7 +144,7 @@ const register = async () => {
 
       if(response.status === 201) {
         setTimeout(() => {
-          router.push("/login")
+          router.push("/dashboard")
         }, 1000)
       } else {
         loader.value = false
@@ -151,12 +183,38 @@ const toggle = () => {
 }
 
 const resetInputs = () => {
-  formData.value.username = ""
-  formData.value.password = ""
-  formData.value.confirmation = ""
+  if(validate()){
+    formData.value.username = ""
+    formData.value.password = ""
+    formData.value.confirmation = ""
+  }
   setTimeout(() => {
       error.value = ""
-    }, 5000)
+    }, 2000)
 }
+
+watchEffect(() => {
+  if(current.value === "username"){
+    for(let validation of usernameValidations.value) {
+      if(validation.regex.test(formData.value.username)){
+        validation.isSuccess = true
+        console.log(validation)
+      } else {
+        validation.isSuccess = false
+        console.log(validation)
+      }
+    }
+  } else {
+    for(let validation of passwordValidations.value) {
+      if(validation.regex.test(formData.value.password)){
+        validation.isSuccess = true
+        console.log(validation)
+      } else {
+        validation.isSuccess = false
+        console.log(validation)
+      }
+    }
+  }
+})
 
 </script>

ebfa74eaa39a49dfc8475ebedc2114c9ea8b9a0e
Chris Dickson C. Molina <cdcmolina@vauldex.com>
2024-05-29 07:58:43 +0800

    Update color theme values J1

--- src/components/organisms/VChatColorSelector.vue
+++ src/components/organisms/VChatColorSelector.vue
@@ -17,21 +17,21 @@
 
 <script setup lang="ts">
 const colorOptions = [
-	"bg-red-500",
-	"bg-orange-500",
-	"bg-amber-500",
-	"bg-yellow-500",
-	"bg-lime-500",
-	"bg-green-500",
-	"bg-emerald-500",
-	"bg-teal-500",
-	"bg-cyan-500",
-	"bg-sky-500",
-	"bg-blue-500",
-	"bg-indigo-500",
-	"bg-violet-500",
-	"bg-purple-500",
-	"bg-fuchsia-500"
+	"bg-red-600",
+	"bg-orange-600",
+	"bg-amber-600",
+	"bg-yellow-600",
+	"bg-lime-600",
+	"bg-green-600",
+	"bg-emerald-600",
+	"bg-teal-600",
+	"bg-cyan-600",
+	"bg-sky-600",
+	"bg-blue-600",
+	"bg-indigo-600",
+	"bg-violet-600",
+	"bg-purple-600",
+	"bg-fuchsia-600"
 	]
 
 const emit = defineEmits<{

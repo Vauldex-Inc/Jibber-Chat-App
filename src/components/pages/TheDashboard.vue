9e8c2eac5ccf34acf38d53e06aafbb93c7136036
iamrcayna <i.am.arcayna@gmail.com>
2024-05-28 09:31:18 +0800

    updated TheDashboard

--- src/components/pages/TheDashboard.vue
+++ src/components/pages/TheDashboard.vue
@@ -1,23 +1,36 @@
-<template>
-	<section class="h-screen">
-		<div class="h-full flex flex-col gap-2">
-			
-		</div>
-	</section>
+ <template>
+ 	<DashboardLayout>
+ 		<template #messages>
+			<VChatList class="h-3/6" title="messages" button-text="Create new channel"/>
+ 		</template>
+ 		 <template #channels>
+			<VChatList class="h-2/6" title="channels"/>
+ 		</template>
+ 		<template #chatbox>
+ 			<VChatTitle name="Vauldex Tech" :count="12"/>
+ 			<VChatBoxArea class="flex-1"/>
+ 			<VChatBox/>
+ 		</template>
+ 		<template #chatinfo>
+ 			<VChatInfo 
+ 					:images="[]"
+ 					:files="[]"
+ 					:members="[]"
+ 					title="channel information"
+ 					name="Vauldex Tech" />
+ 		</template>
+ 	</DashboardLayout>
 </template>
 
 <script lang="ts" setup>
-import VModal from "@/components/atoms/VModal.vue"
-import VAvatar from "@/components/atoms/VAvatar.vue"
-import VSection from "@/components/molecules/VSection.vue"
-import VTextGroup from "@/components/molecules/VTextGroup.vue"
-import VBadge from "@/components/atoms/VBadge.vue"
+import DashboardLayout from "@/components/templates/DashboardLayout.vue"
 import VChatList from "@/components/organisms/VChatList.vue"
+import VChatTitle from "@/components/organisms/VChatTitle.vue"
+import VChatInfo from "@/components/organisms/VChatInfo.vue"
+import VChatListItem from "@/components/organisms/VChatListItem.vue"
+import VChatBox from "@/components/organisms/VChatBox.vue"
+import VChatBoxArea from "@/components/organisms/VChatBoxArea.vue"
 
 
-
-
-import {ref} from "vue"
-
-const open = ref(false)
 </script>
+

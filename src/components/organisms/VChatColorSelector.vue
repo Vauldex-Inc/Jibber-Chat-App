f54faf3276e822441b339786f4d99dbf7e1edaf9
Chris Dickson C. Molina <cdcmolina@vauldex.com>
2024-05-28 14:52:02 +0800

    Add chat color selector J1

---
+++ src/components/organisms/VChatColorSelector.vue
@@ -0,0 +1,41 @@
+<template>
+	<div class="bg-white shadow-md p-5 rounded-md items-center w-[420px] dark:bg-slate-900">
+		<header>
+			<h4 class="pb-2 font-semibold text-gray-600 dark:text-gray-300 text-lg">Select Theme</h4>
+			<hr class="my-3 border-0 border-b border-b-indigo-300 dark:border-b-slate-700"/>
+		</header>
+		<ul class="flex flex-wrap gap-5 p-2">
+			<li 
+					v-for="color in colorOptions" 
+					:key="color" 
+					:class="color"
+					class="w-14 transition-all aspect-square border-4 border-transparent rounded-full hover:border-gray-700 cursor-pointer dark:hover:border-white"  
+					@click="$emit('selectColor', color)"></li>
+		</ul>
+	</div>
+</template>
+
+<script setup lang="ts">
+const colorOptions = [
+	"bg-red-500",
+	"bg-orange-500",
+	"bg-amber-500",
+	"bg-yellow-500",
+	"bg-lime-500",
+	"bg-green-500",
+	"bg-emerald-500",
+	"bg-teal-500",
+	"bg-cyan-500",
+	"bg-sky-500",
+	"bg-blue-500",
+	"bg-indigo-500",
+	"bg-violet-500",
+	"bg-purple-500",
+	"bg-fuchsia-500"
+	]
+
+const emit = defineEmits<{
+	selectColor: []
+}>()
+
+</script>

58e857bed65ca4c62b337c42608d31d448c97067
iamrcayna <i.am.arcayna@gmail.com>
2024-05-29 11:22:35 +0800

    updated channel model

--- src/types/Channel.ts
+++ src/types/Channel.ts
@@ -2,8 +2,8 @@ export interface Channel {
 	id: string;
 	userId: string;
 	title: string;
-	type: string;
-	color: string;
+	channelType: string;
+	color?: string;
 	createdAt: string;
 	archivedAt?: string;
 }

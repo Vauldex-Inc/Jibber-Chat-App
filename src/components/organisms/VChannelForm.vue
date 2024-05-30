cc4dbd3920ac209f3c46668b5decde36a2e8068a
gmmortera <gfmmortera@gmail.com>
2024-05-30 14:49:54 +0800

    Add a create channel form component

--- src/components/organisms/VChannelForm.vue
+++ src/components/organisms/VChannelForm.vue
@@ -1,14 +1,19 @@
 <template>
-	<form @submit.prevent>
-		<VInput size="md" v-model="channelForm.title"/>
-		<VButton size="md" action="submit" @click="createChannel"/>
+	<form @submit.prevent="create" class="flex flex-col gap-4 bg-white shadow-md p-5 rounded-md w-[420px] dark:bg-slate-900">
+		<h4 class="pb-2 font-semibold text-gray-600 dark:text-gray-300 text-lg">Create new channel</h4>
+		<p v-if="error" class="text-red-500 text-center text-sm border border-red-600 bg-red-500/10 p-3 rounded-md">{{ error }}</p>
+		<VInput class="bg-gray-50 rounded-md border-2 w-full pr-12 outline-none border-gray-300 focus:border-indigo-600 hover:border-indigo-600" size="md" v-model="channelForm.title" v-focus placeholder="Channel name"/>
+		<VButton class="bg-indigo-600 text-md text-white rounded-md mt-4" size="md" action="submit">Submit</VButton>
 	</form>
 </template>
 
 <script setup lang="ts">
 import VInput from "@/components/atoms/VInput.vue"
 import VButton from "@/components/atoms/VButton.vue"
-import { ref } from "vue"
+import { Channel } from "@/types/Channel.ts"
+import { useFetch } from "@/composables/useFetch.ts"
+import { useRouter } from "vue-router"
+import { ref, computed } from "vue"
 
 interface ChannelData {
 	title: string
@@ -17,26 +22,51 @@ interface ChannelData {
 	color?: string
 }
 
+const router = useRouter()
+const emits = defineEmits<{
+	submit: [value: Channel | undefined]
+}>()
+
 const error = ref<string>('')
 const channelForm = ref<ChannelData>({
 	title: "",
-	channelType: ""
+	channelType: "MPU"
 })
 
-const createChannel = async () => {
-	try {
-		const response = await useFetch("/channels", {
-			method: "POST",
-			body: JSON.stringify(channelForm.value)
-		})
+const vFocus = {
+  mounted: (el) => el.focus()
+}
 
-		if(response.status !== 200) {
-			error.value = "Failed to create channel."
+
+const create = async () => {
+	try {
+		if(channelForm.value.title) {
+			const response = await useFetch("/channels", {
+				method: "POST",
+				body: JSON.stringify(channelForm.value)
+			})
+
+			if(response.status === 200) {
+				const result = await response.json()
+
+				const channel = result.channel
+
+				const res = await useFetch(`/channels/${channel.id}/users`, {
+					method: "POST"
+				})
+
+				emits('submit', channel)
+			}
+		} else {
+			error.value = "Please provide a channel name."
 		}
 	} catch (err) {
+		emits('submit', undefined)
 		throw new Error(err)
 	} finally {
-		title.value = ""
+		setTimeout(() => {
+			error.value = ""
+		}, 2000)
 	}
 }
 

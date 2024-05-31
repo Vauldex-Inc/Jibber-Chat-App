621edddb06778c91157ef9f2a9683ddb003081b1
gmmortera <gfmmortera@gmail.com>
2024-05-31 11:45:56 +0800

    Add my profile form component to the settings

--- src/components/organisms/VSettings.vue
+++ src/components/organisms/VSettings.vue
@@ -1,7 +1,16 @@
 <template>
+	<VModal :is-open="formShown" @close="formShown = false">
+		<VProfileForm @submit="newProfile" />
+	</VModal>
 	<div class="relative">
 		<VIconButton class="bg-gray-100 dark:bg-slate-900" :invert="true" icon="./src/assets/images/settings.svg" @click="toggleSettings" :rounded="true"/>
 		<ul class="absolute w-[220px] bottom-0 left-0 translate-y-full  flex flex-col bg-white shadow-xl z-30 rounded-md dark:bg-slate-800" v-if="isOpen">
+			<li class="hover:bg-gray-100 dark:hover:bg-slate-700 hover:rounded-md" @click="formShown = true">
+				<VButton class="flex items-center w-full gap-3"  @click="isOpen = false">
+					<img class="h-7 aspect-square" src="@/assets/images/default-avatar.svg" />
+					<span>Profile</span>
+				</VButton>
+			</li>
 			<li class="hover:bg-gray-100 dark:hover:bg-slate-700 hover:rounded-md" @click="toggleSettings">
 					<VThemeSelector />
 			</li>
@@ -23,11 +32,18 @@ import { useFetch } from "@/composables/useFetch"
 import VIconButton from "@/components/atoms/VIconButton.vue"
 import VButton from "@/components/atoms/VButton.vue"
 import VThemeSelector from "@/components/organisms/VThemeSelector.vue"
+import VModal from "@/components/atoms/VModal.vue"
+import VProfileForm from "@/components/organisms/VProfileForm.vue"
 
 const router = useRouter()
 const isOpen = ref<boolean>(false)
+const formShown = ref<boolean>(false)
 
 const toggleSettings = () => isOpen.value = !isOpen.value
+
+const newProfile = (profile: Profile | undefined) => {
+	formShown.value = false
+}
 	
 const logout = async () => {
 	try {

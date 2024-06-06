ac2d91ac9216a02487011dc9c83c921239301077
iamrcayna <i.am.arcayna@gmail.com>
2024-06-06 13:49:40 +0800

    added route guard

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import TheLogin from '@/components/pages/TheLogin.vue'
 import TheRegister from '@/components/pages/TheRegister.vue'
 import TheDashboard from "@/components/pages/TheDashboard.vue"
+import {useUser} from "@/composables/useUser"
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -23,6 +24,14 @@ const router = createRouter({
     {
       path: '/dashboard',
       name: 'dashboard',
+      beforeEnter: (_,__,next) => {
+        const user = useUser()
+        if(user) {
+          next()
+        } else {
+          next({ name: 'login' })
+        }
+      },
       component: TheDashboard
     }
   ]

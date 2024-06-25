dd29e9bd516726b3e88b76b315de6059c1239236
tjjvirtudazo <tjjvirtudazo@vauldex.com>
2024-06-25 10:34:04 +0800

    Refactor channel model

--- src/types/Channel.ts
+++ src/types/Channel.ts
@@ -1,14 +1,26 @@
-export interface Channel {
-	id: string;
-	userId: string;
-	title: string;
-	channelType: string;
-	color?: string;
-	createdAt: string;
-	archivedAt?: string;
-}
+import { z } from 'zod'
 
-export interface ChannelUser {
-	channelId: string;
-	userId: string
+const ChannelSchema = z.object({
+	id: z.string(),
+	userId: z.string(),
+	title: z.string(),
+	channelType: z.string(),
+	color: z.string().optional(),
+	createdAt: z.string(),
+	archivedAt: z.string().optional() 
+})
+
+const ChannelUserSchema = z.object({
+	channelId: z.string(),
+	userId: z.string()
+})
+
+type Channel = z.infer<typeof ChannelSchema>
+type ChannelUser = z.infer<typeof ChannelUserSchema>
+
+export { 
+	type Channel,
+	type ChannelUser,
+	ChannelSchema,
+	ChannelUserSchema
 }

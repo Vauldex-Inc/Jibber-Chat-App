005615e6b61cab047759cca147a6a4630afa4f47
tjjvirtudazo <tjjvirtudazo@vauldex.com>
2024-06-26 13:26:27 +0800

    Replace img element to VImage component

--- src/components/atoms/VIconButton.vue
+++ src/components/atoms/VIconButton.vue
@@ -6,17 +6,15 @@
     type="button"
   >
     <div class="relative">
-      <img
-        :src="icon"
-        alt="icon"
-        :class="[
-          customImageSizeClass(size),
-          invert ? 'invert dark:invert-0' : 'invert-0',
-        ]"
-        class="aspect-square rounded-full object-cover"
+      <VImage 
+        :source="icon"
+        type="icon"
+        :invert="invert"
       />
       <p
-        class="absolute -bottom-4 left-1/2 z-50 hidden min-w-24 -translate-x-1/2 translate-y-full rounded-md bg-slate-800 px-2 py-1 text-sm capitalize text-gray-200 group-hover:block"
+        class="absolute -bottom-4 left-1/2 z-50 hidden min-w-24 -translate-x-1/2 
+        translate-y-full rounded-md bg-slate-800 px-2 py-1 text-sm capitalize text-gray-200 
+        group-hover:block"
         v-if="toolTip"
       >
         {{ toolTip }}
@@ -27,12 +25,16 @@
 
 <script setup lang="ts">
 import { computed } from "vue"
+
 import { sizeClass, customImageSizeClass } from "@/composables/useSize"
+
+import { type Size }  from "@/types/Component"
+import VImage from "@/components/atoms/VImage.vue"
 
 const props = defineProps<IconButtonProps>()
 
 interface IconButtonProps {
-  size?: "small" | "medium" | "large" | "extraLarge"
+  size: Size
   icon: string
   invert?: boolean
   rounded?: boolean

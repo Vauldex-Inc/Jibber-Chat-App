b5c682e5f4f43559615df95a5e30227cbb604d40
tjjvirtudazo <tjjvirtudazo@vauldex.com>
2024-06-26 13:27:00 +0800

    Add VUserStatus component

---
+++ src/components/atoms/VUserStatus.vue
@@ -0,0 +1,26 @@
+<template>
+  <div
+    :class="statusClass"
+    class="absolute bottom-0 right-0 z-10 aspect-square h-5 translate-x-1 
+    translate-y-1 rounded-full border-[3px] border-gray-100 dark:border-slate-900"
+  ></div>
+</template>
+
+<script lang="ts" setup>
+import { computed } from 'vue'
+
+const prop = defineProps<{
+  status?: string
+}>()
+
+const statusClass = computed(() => {
+  switch (prop.status) {
+    case "online":
+      return "bg-emerald-500"
+    case "offline":
+      return "bg-slate-400 dark:bg-slate-600"
+    default:
+      return "bg-slate-400 dark:bg-slate-600"
+  }
+})
+</script>

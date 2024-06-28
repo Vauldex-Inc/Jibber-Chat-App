29c5b170694630f6ca7db8659da78a3a90c7c9ee
tjjvirtudazo <tjjvirtudazo@vauldex.com>
2024-06-28 09:38:00 +0800

    Rename id

--- src/types/Message.ts
+++ src/types/Message.ts
@@ -2,19 +2,19 @@ import { z } from 'zod'
 
 const MessageSchema = z.object({
 	id: z.string(),
-	channelId: z.string(),
-	userId: z.string(),
+	idChannel: z.string(),
+	idUser: z.string(),
 	text: z.string(),
 	image: z.string().optional(),
 	sentAt: z.string()
 })
 
 const UnreadMessageSchema = z.object({
-	messageId: z.string(),
-	channelId: z.string(),
-	userId: z.string(),
+	idMessage: z.string(),
+	idChannel: z.string(),
+	idUser: z.string(),
 	unreads: z.string(),
-	senderId: z.string(),
+	idSender: z.string(),
 	sentAt: z.string(),
 	text: z.string()
 })

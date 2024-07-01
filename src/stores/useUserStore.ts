f989a5e811c8d1a90574f7a2c8ab51e8c10e1342
tjjvirtudazo <tjjvirtudazo@vauldex.com>
2024-07-01 16:59:31 +0800

    Fix code

--- src/stores/useUserStore.ts
+++ src/stores/useUserStore.ts
@@ -40,59 +40,6 @@ export const useUserStore = defineStore("users", () => {
 		return users.value.find((user) => user.id == idUser)
 	}
 
-	const getUserNameById = (id: string) => {
-		const userProfile = getUser(id)
-
-		if (!userProfile) {
-			return "Anonymous"
-		}
-
-		const user = userProfile.user
-		const profile = userProfile.profile
-
-		if (!profile) {
-			return user?.username
-		}
-
-		if (!profile.firstName && !profile.lastName) {
-			return user?.username
-		}
-
-		if (profile.firstName?.trim() === "" || profile.lastName?.trim() === "") {
-			return user?.username
-		}
-
-		return `${profile.firstName} ${profile.lastName}`
-	}
-
-	const getUserImageById = (id: string) => {
-		const userProfile = getUser(id)
-
-		if (!userProfile) {
-			return undefined
-		}
-
-		const user = userProfile.user
-		const profile = userProfile.profile
-
-		if (!profile) {
-			return undefined
-		}
-		return profile.image
-	}
-
-	const addUserProfile = (id: string, newProfile: Profile) => {
-		const updatedUsers = [...users.value.map(u => {
-			const [user, profile] = u
-			if (user.id === id) {
-				return [user, newProfile] as [User, Profile]
-			}
-			return [user, profile] as [User, Profile]
-		})]
-
-		users.value = [...updatedUsers]
-	}
-
 	const updateUserOnlineAt = (users: string[], type: string) => {
 		if (type === "online") {
 			onlineUsers.value = users
@@ -106,26 +53,11 @@ export const useUserStore = defineStore("users", () => {
 	}
 
 	const addNewUser = (user: User) => {
-		users.value.push([user, undefined])
+		users.value.push(user)
 	}
 
 	const sentAtFormatter = (message: Message, options: Intl.DateTimeFormatOptions = defaultOptions) => {
 		return dateFormatter.format(message.sentAt, options)
-	}
-
-	const senderName = (userId?: string) => {
-		return userId ? getUserNameById(userId) : "Anonymous";
-	};
-
-	const senderProfile = (userId: string) => {
-		return userId ? getUserImageById(userId) : undefined;
-	};
-
-	const senderStatus = (userId?: string) => {
-		if (userId && getOnlineUsers().value.includes(userId)) {
-			return "online"
-		}
-		return "offline"
 	}
 
 	const getStatus = (userId?: string) => {
@@ -147,9 +79,7 @@ export const useUserStore = defineStore("users", () => {
 	}
 
 	return {
-		users, init, getUser, getUsers, getUserNameById,
-		getUserImageById, addUserProfile, updateUserOnlineAt, onlineUsers,
-		getOnlineUsers, addNewUser, sentAtFormatter, senderName, senderProfile,
-		senderStatus, getStatus, inviteMember
+		users, init, getUser, getUsers, getStatus, updateUserOnlineAt, onlineUsers,
+		getOnlineUsers, addNewUser, sentAtFormatter, inviteMember
 	}
 })

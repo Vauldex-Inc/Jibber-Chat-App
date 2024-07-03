b7161022bf3f808a8a592760498ed16c269ea689
Gianfranco M. Mortera <gmmortera@vauldex.com>
2024-07-03 12:49:17 +0800

    Update chat list component

--- src/components/organisms/ChatListItem.vue
+++ src/components/organisms/ChatListItem.vue
@@ -21,6 +21,7 @@
       <VAvatar
         :image="profileStore.getImage(user)"
         :status="userStore.getStatus(user)"
+        size="extraLarge"
       />
       <VTextGroup
         :is-bold="unReadMessages.length > 0"

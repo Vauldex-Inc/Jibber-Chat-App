b4b1d93cf094015789c07ec34b68f5374af8c646
tjjvirtudazo <tjjvirtudazo@vauldex.com>
2024-07-03 13:48:13 +0800

    Refactor channel domain

--- src/types/Channel.ts
+++ src/types/Channel.ts
@@ -1,13 +1,19 @@
 import { z } from 'zod'
 
+const GROUP_CHANNEL = "MPU"
+const DIRECT_CHANNEL = "SNG"
+const PRIVATE_CHANNEL = "MPR"
+
+const ChannelVariantEnum = z.enum([GROUP_CHANNEL, DIRECT_CHANNEL, PRIVATE_CHANNEL])
+
 const ChannelSchema = z.object({
 	id: z.string(),
 	idUser: z.string(),
 	title: z.string(),
-	channelType: z.string().optional(),
+	channelType: ChannelVariantEnum.optional(),
 	color: z.string().optional(),
-	createdAt: z.string(),
-	archivedAt: z.string().optional()
+	createdAt: z.string().datetime(),
+	archivedAt: z.string().datetime().optional()
 })
 
 const ChannelUserSchema = z.object({
@@ -23,15 +29,31 @@ const DirectChannelSchema = z.object({
 	idReceiver: z.string().uuid()
 })
 
+const ChannelDataSchema = z.object({
+  title: z.string(),
+  channelType: ChannelVariantEnum,
+  archivedAt: z.string().optional(),
+  color: z.string().optional()
+})
+
+type ChannelVariant = z.infer<typeof ChannelVariantEnum>
+
 type Channel = z.infer<typeof ChannelSchema>
 type ChannelUser = z.infer<typeof ChannelUserSchema>
 type DirectChannel = z.infer<typeof DirectChannelSchema>
+type ChannelData = z.infer<typeof ChannelDataSchema>
 
 export {
 	type Channel,
 	type ChannelUser,
 	type DirectChannel,
+	type ChannelData,
+	type ChannelVariant,
+	ChannelVariantEnum,
+	ChannelDataSchema,
 	ChannelSchema,
 	ChannelUserSchema,
-	DirectChannelSchema
+	DirectChannelSchema,
+	GROUP_CHANNEL,
+	DIRECT_CHANNEL
 }

60fb978c68f8642d46f7d42d82eba1a7135d9153
Gianfranco M. Mortera <gmmortera@vauldex.com>
2024-07-03 15:09:05 +0800

    Add interface for props

--- src/types/Prop.ts
+++ src/types/Prop.ts
@@ -1,10 +1,68 @@
+import { type Size } from "@/types/Component"
 interface AvatarProp {
   image: string
   type: string
   size: string
-  status: string
+  status: "offline" | "online"
+}
+interface BadgeProp {
+  color?: string
+  count: number
 }
 
-export {
-  type AvatarProp
+interface ButtonProp {
+  size?: "small" | "medium" | "large"
+  action?: "submit" | "button"
+}
+
+interface ImageProp {
+  source?: string
+  type: string
+  size?: string
+  invert?: boolean
+}
+
+interface IconButtonProp {
+  size?: Size
+  icon: string
+  type?: string
+  invert?: boolean
+  rounded?: boolean
+  hasPadding?: boolean
+  toolTip?: string
+}
+
+interface InputProp {
+  type?: "text" | "file" | "password"
+  size?: "small" | "medium" | "large"
+}
+
+interface ModalProp {
+  isOpen: boolean
+  closeButton?: boolean
+}
+
+interface TextGroupProp {
+  title: string
+  text: string
+  subText?: string
+  isBold?: boolean
+}
+
+interface ToastProp {
+  toastId: string
+  message: string
+  title: string
+}
+
+export type {
+  AvatarProp,
+  BadgeProp,
+  ButtonProp,
+  IconButtonProp,
+  ImageProp,
+  InputProp,
+  ModalProp,
+  TextGroupProp,
+  ToastProp
 }
